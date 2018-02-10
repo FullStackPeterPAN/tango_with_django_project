499317b92c490f45c0ae8b442523779d2e4caa52
Yangjiangdai Pan <2294163p@student.gla.ac.uk>
2018-02-10 16:02:19 +0000

    chapter 10 1

--- rango/views.py
+++ rango/views.py
@@ -13,21 +13,14 @@ from datetime import datetime
 # Create your views here.
 
 def index(request):
-
     request.session.set_test_cookie()
-      
     category_list = Category.objects.order_by('-likes')[:5]
-    
     page_list = Page.objects.order_by('-views')[:5]
-    
     context_dict = {'categories': category_list, 'pages': page_list}
-
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
-    
     response = render(request, 'rango/index.html', context=context_dict)
     return response
-    
 
 def about(request):
 
@@ -248,29 +241,29 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
-
-
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
     if not val:
         val = default_val
     return val
 
-
+# Updated the function definition
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits','1'))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
 
+    # If it's been more than a day since the last visit...
     if (datetime.now() - last_visit_time).days > 0:
-        visits = visits +1
-
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
-        
     else:
         visits = 1
+        # set the last visit cookie
         request.session['last_visit'] = last_visit_cookie
 
+    # Update/set the visits cookie
     request.session['visits'] = visits
     
